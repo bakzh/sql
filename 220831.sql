91f5ed1f18a43bcaba3521ad0dae30f223a9f1c9
bakzh <park3096118@gmail.com>
2022-08-31 16:51:34 +0900

    220831 -8

---
+++ 220831.sql
@@ -0,0 +1,27 @@
+select * from product;
+
+select product_id, pname, quantity, price
+  from product
+ where product_id = 42 ;
+ 
+update product
+   set pname = '마우스패드',
+       quantity = 10,
+       price = 10000
+ where product_id = 42;      
+       
+delete from product where product_id = 41; 
+
+rollback;
+
+select product_id, pname, quantity, price
+  from product;
+  
+delete from product;
+commit;
+
+select product_product_id_seq.nextval
+  from dual;
+
+select 'm-' || product_product_id_seq.nextval
+  from dual;
